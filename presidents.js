c258f5c62fd34287e20f873956f7f161333a3431
ScorpionBz <scorpbz.oficial@gmail.com>
2026-07-13 00:18:33 -0600

    Add president styles with pressure and priority

---
+++ presidents.js
@@ -0,0 +1,41 @@
+[]
+const PRESIDENT_STYLE={
+
+
+"Florentino Pérez":{
+
+pressure:"muy alta",
+
+priority:"Champions League"
+
+},
+
+
+"Joan Laporta":{
+
+pressure:"alta",
+
+priority:"cantera y títulos"
+
+},
+
+
+"Sheikh Mansour":{
+
+pressure:"media",
+
+priority:"ganar todo"
+
+},
+
+
+"Nasser Al-Khelaifi":{
+
+pressure:"alta",
+
+priority:"estrellas mundiales"
+
+}
+
+
+};
