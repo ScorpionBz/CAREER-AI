5b050467d69ef9ac05a63c9d24826db45ea3b9b3
ScorpionBz <scorpbz.oficial@gmail.com>
2026-07-13 00:27:30 -0600

    Create FC25_DATABASE with team and player information
    Added FC25_DATABASE containing teams, players, and their details.

---
+++ data/database_fc25.js
@@ -0,0 +1,75 @@
+const FC25_DATABASE = {
+
+
+"Real Madrid":{
+
+league:"LaLiga",
+
+budget:150000000,
+
+stadium:"Santiago Bernabéu",
+
+captain:"Luka Modric",
+
+players:[
+
+{
+name:"Vinicius Jr",
+position:"LW",
+overall:90,
+potential:92,
+age:24,
+role:"Estrella"
+},
+
+{
+name:"Jude Bellingham",
+position:"CAM",
+overall:90,
+potential:94,
+age:21,
+role:"Estrella"
+}
+
+]
+
+},
+
+
+
+"FC Barcelona":{
+
+league:"LaLiga",
+
+budget:100000000,
+
+stadium:"Spotify Camp Nou",
+
+captain:"Marc-André ter Stegen",
+
+players:[
+
+{
+name:"Lamine Yamal",
+position:"RW",
+overall:81,
+potential:94,
+age:17,
+role:"Promesa"
+},
+
+{
+name:"Pedri",
+position:"CM",
+overall:86,
+potential:91,
+age:22,
+role:"Titular"
+}
+
+]
+
+}
+
+
+};
